ebc7134ece1b8251eb5eac979fb977678ae3cb4f
Adekunle <akinhanmiadekunle@gmail.com>
2025-10-11 20:53:08 +0100

    (My second commit)

--- week-2/practice_2.rs
+++ week-2/practice_2.rs
@@ -2,5 +2,5 @@ fn main(){
 	println!(); // prints just a
 	println!("hello");//prints
 	println!("format {} arguments", "some"); //prints format some arguments
-	println!("My name is {}. I am the class rep of Stream 1", "Dagogo William-Jumbo")
+	println!("My name is {}. I am the class rep of Stream 1", "Dagogo William-Jumbo");
 }
